2fa48eda28d1cb8e8528a80669c882aad14aee82
Brainiac2025 <afeez.pierscon@gmail.com>
2023-06-09 22:56:20 +0100

    Variable Declaration set up

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+
+    Variables myVariable= new Variables();
+
+    private void initiateGame(){
+        System.out.println("WELCOME TO DUNGEON ADVENTURE GAME");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Variables {
+   protected Scanner input= new Scanner(System.in);
+   protected Random rnd= new Random();
+
+   protected int enemy_max_health=80;
+   protected int enemy_attack_damage=30;
+
+   protected int player_health_max=100;
+   protected int player_damage=25;
+   protected int num_health_potion=4;
+   protected int health_potion_heal_amount=30;
+   protected int health_potion_drop_chance=50;
+
+   protected boolean gameOn=true;
+
+   String[]enemies={"Assasin", "Skeleton", "Warrior", "Zombie"};
+
+
+
+}
