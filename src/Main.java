c1e1c475999bbfc3f0b113ec2c98409d4d436989
Brainiac2025 <afeez.pierscon@gmail.com>
2023-06-12 17:05:28 +0100

    make use of if statment to  check if the player is dead

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
                     System.out.println("\t> You Strike the "+enemy+"for"+damagedone+" damage. ");
                     System.out.println("\t> You receive "+damagetaken+" in retaliation");
 
-                    if(myVariable.player_health<=0) {
+                    if(myVariable.player_health<1) {
                         System.out.println("\t> You have taken Too much Damage\n You " +
                                 "are too weak to continue the game");
                         break;
@@ -38,17 +38,43 @@ public class Main {
                         myVariable.player_health+=myVariable.health_potion_heal_amount;
                         myVariable.num_health_potion--;
                         System.out.println("\t> You drink a health potion of "+myVariable.health_potion_heal_amount
-                        +"\n\t Your current health "+myVariable.player_health+ " HP.");
+                        +"\n\t You have "+myVariable.player_health+ " amount of HP."+ "\n\t> You have "+myVariable.num_health_potion+
+                                " Health Potion Left \n");
+                    }
+                    else{
+                        System.out.println("\t> You have no health potion left!!\n" +
+                                "Defeat your ennemy for a chance to get one!!!");
                     }
 
                 }
                 else if(myinput.equals("3")){
+                    System.out.println("\t> You are not Strong!!!\n\t You run away from the enemy "+enemy+" !!");
+                    continue;
 
                 }
                 else{
-
+                    System.out.println("\t> You have enter an invalid Command!!");
                 }
             }
+            if(myVariable.player_health<1){
+                System.out.println("You are not strong to play the battle!!");
+                break;
+            }
+            System.out.println("__________________________________");
+            System.out.println("# "+enemy+" was defeated! #");
+            System.out.println("# You have "+myVariable.player_health+" HP left #");
+
+            if (myVariable.rnd.nextInt(100)< myVariable.health_potion_drop_chance){
+                myVariable.num_health_potion++;
+                System.out.println("# The "+ enemy+ " dropped a health potion! #");
+                System.out.println("# You have "+myVariable.num_health_potion+" Health potion #");
+
+            }
+            System.out.println("___________________________________");
+            System.out.println("What would you like to do now? ");
+            System.out.println("1 Continue Fighting? ");
+            System.out.println("2 Exit Game");
+
 
         }
 
